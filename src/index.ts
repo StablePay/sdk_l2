b82044aaed280863d16f59fdd37e8b78a2a20f65
Guillermo Daniel Salazar <guillesalazar@gmail.com>
2020-07-17 22:53:16 -0300

    Linting

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-export { StablePayLayer2Manager as Layer2Manager } from './StablePayLayer2Manager'
+export { StablePayLayer2Manager as Layer2Manager } from './StablePayLayer2Manager';
 export * from './types';
