5a95c1d5cbe5a3f05f12ef46dd06d7e474ee32b3
Rodimiro Cerrato E <rodyce@gmail.com>
2020-08-24 00:17:49 -0600

    Add TRANSFER and WITHDRAW unit tests
    Signed-off-by: Rodimiro Cerrato E <rodyce@gmail.com>

--- test/zksyncWallet.spec.ts
+++ test/zksyncWallet.spec.ts
@@ -34,7 +34,7 @@ describe('zkSync Wallet-related functionality testing', () => {
   const network: Network = 'rinkeby';
 
   // Common setup.
-  beforeAll(async () => {
+  beforeEach(async () => {
     ethersSigner = buildMockSigner();
     zkSyncWallet = buildMockWallet();
     zkSyncProvider = buildMockProvider();
@@ -79,9 +79,10 @@ describe('zkSync Wallet-related functionality testing', () => {
 
     (layer2Wallet as any).upgradeToSigningWallet = async () => {
       (layer2Wallet as any).isSigningWallet = true;
-      return new Promise((resolve) => {
-        resolve();
-      });
+      // return new Promise((resolve) => {
+      //   resolve();
+      // });
+      return Promise.resolve();
     };
 
     // Mock unlockAccount to simulate account unlock. Need to cast to any
@@ -94,9 +95,7 @@ describe('zkSync Wallet-related functionality testing', () => {
       });
     };
 
-    let syncTransferCallCount = 0;
     zkSyncWallet.syncTransfer.mockImplementation(async () => {
-      syncTransferCallCount++;
       if (accountLocked) {
         // Simulate locked account.
         throw new Error('Account is locked.');
@@ -112,5 +111,237 @@ describe('zkSync Wallet-related functionality testing', () => {
     // Expectations.
     expect(accountLocked).toBeFalsy();
     expect(zkSyncWallet.syncTransfer).toHaveBeenCalledTimes(2);
+  });
+
+  it('should throw exception immediately on unlocked account when TRANSFER txn', async () => {
+    // Test setup.
+    // Start with a UNLOCKED account.
+    let accountLocked = false;
+    // Transfer data.
+    const toAddress = SAMPLE_ADDRESS;
+    const transferFee = '0.01';
+    const transfer = new Transfer({
+      toAddress,
+      amount: '0.1', // Desired amount to withdraw.
+      fee: transferFee, // Desired fee to pay. This is a LAYER TWO fee.
+      tokenSymbol: 'ETH',
+    });
+
+    // Set as signing wallet.
+    (layer2Wallet as any).isSigningWallet = true;
+
+    // Mock unlockAccount to simulate account unlock. Need to cast to any
+    // since this method is private (TS).
+    (layer2Wallet as any).unlockAccount = mockDeep();
+
+    const ERROR_MSG = 'Metamask Cancel';
+    zkSyncWallet.syncTransfer.mockImplementation(async () => {
+      if (accountLocked) {
+        // Simulate locked account.
+        throw new Error('Account is locked.');
+      } else {
+        // Throw dummy exception.
+        throw new Error(ERROR_MSG);
+      }
+    });
+
+    // Method under test.
+    const transferFn = async () => await layer2Wallet.transfer(transfer);
+
+    // Expectations.
+    await expect(transferFn).rejects.toThrow(ERROR_MSG);
+    expect(zkSyncWallet.syncTransfer).toHaveBeenCalledTimes(1);
+  });
+
+  it('should unlock account if locked on WITHDRAW txn', async () => {
+    // Test setup.
+    // Start with a locked account.
+    let accountLocked = true;
+    // Transfer data.
+
+    // Withdraw back to the LAYER 1 wallet's address.
+    const myAddress = layer2Wallet.getAddress();
+
+    // A withdrawal fee from LAYER TWO.
+    const withdrawalFee = '0.01';
+
+    const withdrawal = new Withdrawal({
+      toAddress: myAddress,
+      amount: '0.1', // Desired amount to withdraw.
+      fee: withdrawalFee, // Desired fee to pay. This is a LAYER TWO fee.
+      tokenSymbol: 'ETH',
+    });
+
+    (layer2Wallet as any).upgradeToSigningWallet = async () => {
+      (layer2Wallet as any).isSigningWallet = true;
+      return new Promise((resolve) => {
+        resolve();
+      });
+    };
+
+    // Mock unlockAccount to simulate account unlock. Need to cast to any
+    // since this method is private (TS).
+    (layer2Wallet as any).unlockAccount = () => {
+      // Simulate account unlock.
+      accountLocked = false;
+      return new Promise((resolve) => {
+        resolve();
+      });
+    };
+
+    zkSyncWallet.withdrawFromSyncToEthereum.mockImplementation(async () => {
+      if (accountLocked) {
+        // Simulate locked account.
+        throw new Error('Account is locked.');
+      } else {
+        // Return dummy value.
+        return {} as any;
+      }
+    });
+
+    // Method under test. Perform WITHDRAW operation.
+    await layer2Wallet.withdraw(withdrawal);
+
+    // Expectations.
+    expect(accountLocked).toBeFalsy();
+    expect(zkSyncWallet.withdrawFromSyncToEthereum).toHaveBeenCalledTimes(2);
+  });
+
+  it('should throw exception immediately on unlocked account when WITHDRAW txn', async () => {
+    // Test setup.
+    // Start with a UNLOCKED account.
+    let accountLocked = false;
+
+    // Withdraw back to the LAYER 1 wallet's address.
+    const myAddress = layer2Wallet.getAddress();
+
+    // A withdrawal fee from LAYER TWO.
+    const withdrawalFee = '0.01';
+
+    const withdrawal = new Withdrawal({
+      toAddress: myAddress,
+      amount: '0.1', // Desired amount to withdraw.
+      fee: withdrawalFee, // Desired fee to pay. This is a LAYER TWO fee.
+      tokenSymbol: 'ETH',
+    });
+
+    // Set as signing wallet.
+    (layer2Wallet as any).isSigningWallet = true;
+
+    // Mock unlockAccount to simulate account unlock. Need to cast to any
+    // since this method is private (TS).
+    (layer2Wallet as any).unlockAccount = mockDeep();
+
+    const ERROR_MSG = 'Metamask Cancel';
+    zkSyncWallet.withdrawFromSyncToEthereum.mockImplementation(async () => {
+      if (accountLocked) {
+        // Simulate locked account.
+        throw new Error('Account is locked.');
+      } else {
+        // Throw dummy exception.
+        throw new Error(ERROR_MSG);
+      }
+    });
+
+    // Method under test.
+    const transferFn = async () => await layer2Wallet.withdraw(withdrawal);
+
+    // Expectations.
+    await expect(transferFn).rejects.toThrow(ERROR_MSG);
+    expect(zkSyncWallet.withdrawFromSyncToEthereum).toHaveBeenCalledTimes(1);
+  });
+
+  it('should throw exception immediately on unlocked account when WITHDRAW txn', async () => {
+    // Test setup.
+    // Start with a UNLOCKED account.
+    let accountLocked = false;
+
+    // Withdraw back to the LAYER 1 wallet's address.
+    const myAddress = layer2Wallet.getAddress();
+
+    // A withdrawal fee from LAYER TWO.
+    const withdrawalFee = '0.01';
+
+    const withdrawal = new Withdrawal({
+      toAddress: myAddress,
+      amount: '0.1', // Desired amount to withdraw.
+      fee: withdrawalFee, // Desired fee to pay. This is a LAYER TWO fee.
+      tokenSymbol: 'ETH',
+    });
+
+    // Set as signing wallet.
+    (layer2Wallet as any).isSigningWallet = true;
+
+    // Mock unlockAccount to simulate account unlock. Need to cast to any
+    // since this method is private (TS).
+    (layer2Wallet as any).unlockAccount = mockDeep();
+
+    const ERROR_MSG = 'Metamask Cancel';
+    zkSyncWallet.withdrawFromSyncToEthereum.mockImplementation(async () => {
+      if (accountLocked) {
+        // Simulate locked account.
+        throw new Error('Account is locked.');
+      } else {
+        // Throw dummy exception.
+        throw new Error(ERROR_MSG);
+      }
+    });
+
+    // Method under test.
+    const transferFn = async () => await layer2Wallet.withdraw(withdrawal);
+
+    // Expectations.
+    await expect(transferFn).rejects.toThrow(ERROR_MSG);
+    expect(zkSyncWallet.withdrawFromSyncToEthereum).toHaveBeenCalledTimes(1);
+    //expect((layer2Wallet as any).unlockAccount).not.toHaveBeenCalled();
+  });
+
+  it('WITHDRAW txn happy path', async () => {
+    // Test setup.
+
+    // Withdraw back to the LAYER 1 wallet's address.
+    const myAddress = layer2Wallet.getAddress();
+
+    // A withdrawal fee from LAYER TWO.
+    const withdrawalFee = '0.01';
+
+    const withdrawal = new Withdrawal({
+      toAddress: myAddress,
+      amount: '0.1', // Desired amount to withdraw.
+      fee: withdrawalFee, // Desired fee to pay. This is a LAYER TWO fee.
+      tokenSymbol: 'ETH',
+    });
+
+    // Set as signing wallet.
+    (layer2Wallet as any).isSigningWallet = true;
+
+    // ? await this.resultHolder.awaitReceiptVerify()
+    // : await this.resultHolder.awaitReceipt();
+
+    const fakeReceipt = {
+      block: {
+        blockNumber: 12,
+        committed: true,
+        verified: false,
+      },
+    } as any;
+
+    zkSyncWallet.withdrawFromSyncToEthereum.mockImplementation(async () => {
+      const fn = () => Promise.resolve(fakeReceipt);
+      return {
+        awaitReceipt: fn,
+        awaitReceiptVerify: fn,
+      } as any;
+    });
+
+    // Method under test.
+    const result = await layer2Wallet.withdraw(withdrawal);
+    const receipt = await result.getReceipt();
+
+    console.log(receipt);
+
+    // Expectations.
+    expect(zkSyncWallet.withdrawFromSyncToEthereum).toHaveBeenCalledTimes(1);
+    expect(receipt.operationType).toEqual(OperationType.Withdrawal);
   });
 });
